7ffc005fc4bc711cb1b06843f40441ec711ab2eb
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-07-11 15:42:31 +0300

    final commit

--- app/src/main/java/com/example/first2dgame/InstructionsActivity.java
+++ app/src/main/java/com/example/first2dgame/InstructionsActivity.java
@@ -10,7 +10,7 @@ public class InstructionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
-        
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
